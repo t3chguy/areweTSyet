a32851eac2fe18727fcdb4ada75215ecc18e33e2
Michael Telatynski <7t3chguy@gmail.com>
2020-10-05 17:58:11 +0100

    Convert into a generic TS/JS conversion tracker for the eleweb team

--- script.py
+++ script.py
@@ -1,81 +1,100 @@
+import pathlib
+import re
+import sys
 from collections import defaultdict
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 import json
 import subprocess
 
 from git import Repo
 
-# Constants.
-INITIAL_COMMIT = "4f475c7697722e946e39e42f38f3dd03a95d8765"
-SYNAPSE_DIR = Path(".") / "synapse"
+REPO_NAMES = [
+    "vector-im/element-desktop",
+    "vector-im/element-web",
+    "matrix-org/matrix-react-sdk",
+    "matrix-org/matrix-js-sdk",
+]
 
-# Start at the nearest Monday.
 day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-while day.weekday():
-    day -= timedelta(days=1)
 
-repo = Repo(SYNAPSE_DIR)
+BASE_PATH = Path("./repos")
 
-# Fetch updated changes.
-origin = repo.remotes[0]
-origin.fetch()
-
-def search(string):
-    result = subprocess.run(
-        ["grep", "-rE", "\\b" + string + "\\b", "synapse", "tests", "contrib", "docs", "scripts"],
-        capture_output=True,
-        cwd=SYNAPSE_DIR)
-
-    total = 0
-    by_module = defaultdict(int)
-    for line in result.stdout.splitlines():
-        filename, match = line.split(b":", 1)
-        filename = filename.decode("ascii")
-        module = "/".join(filename.split("/", 2)[:2])
-        # If there's a file extension, strip it off.
-        module = module.split(".")[0]
-        total += 1
-        by_module[module] += 1
-
-    return total, by_module
+regex = re.compile(r"(\w+).+?(\d+)")
 
 
-# Iterate from the newest to the oldest commit.
-data = []
-for it, commit in enumerate(repo.iter_commits("origin/develop")):
-    # Get the commit at the start of the day.
-    committed_date = datetime.fromtimestamp(commit.committed_date)
-    # Always include the latest commit, the earliest commit, and the last commit
-    # of each Sunday.
-    if committed_date < day or commit.hexsha == INITIAL_COMMIT or it == 0:
-        # The next date will be a week in the past, if this is not the initial
-        # commit.
-        if it != 0:
-            day -= timedelta(days=7)
+def get_repo(repo_name):
+    path = BASE_PATH / repo_name
+    try:
+        pathlib.Path(path).mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        pass
+    else:
+        Repo.clone_from(f"https://github.com/{repo_name}", path)
 
-        # Checkout this commit (why is this so hard?).
-        repo.head.reference = commit
-        repo.head.reset(index=True, working_tree=True)
+    return Repo(path)
 
-        # Find the number of inlineCallback functions.
-        inlineCallbacks_result = search("(inlineCallbacks|cachedInlineCallbacks)")
 
-        # Additional helpers.
-        deferreds_results = search("(ensureDeferred|maybeDeferred|succeed|failure)\\(")
+def scan(repo):
+    args = [
+        "sloc", f"{repo.working_dir}/src",
+        "--format", "cli-table",
+        "--keys", "source",
+        "--format-option", "no-head",
+    ]
+    print("Scanning", repo.working_dir)
 
-        # Find the number of async functions.
-        async_result = search("async def")
+    by_extension = defaultdict(int)
+    try:
+        res = subprocess.run(
+            args,
+            capture_output=True,
+            shell=True,
+            cwd=repo.working_dir)
 
-        print(commit, inlineCallbacks_result[0], deferreds_results[0], async_result[0])
+        for line in res.stdout.splitlines():
+            l = line.decode("utf-8")[2:-2]
+            r = regex.match(l)
+            if not r:
+                continue
 
-        data.append((
-            commit.hexsha,
-            str(committed_date),
-            inlineCallbacks_result,
-            deferreds_results,
-            async_result,
-        ))
+            # print(l)
+            # print(r.groups())
+
+            by_extension[r.group(1)] += int(r.group(2))
+    except FileNotFoundError as not_found:
+        print("FileNotFoundError", repo.working_dir, not_found.filename, not_found.filename2)
+
+    return by_extension
+
+
+REPOS = list(map(get_repo, REPO_NAMES))
+
+results = []
+for i, repo in enumerate(REPOS):
+    repo.remotes[0].pull()
+
+    commit = repo.head.commit
+    result = scan(repo)
+
+    print(commit, result)
+
+    results.append(dict(result))
+
+with open("results.json", "r+") as f:
+    try:
+        existing_data = json.load(f) or []
+    except:
+        existing_data = []
+
+existing_data.append((
+    str(day.date()),
+    [(
+        REPO_NAMES[i],
+        REPOS[i].head.commit.hexsha,
+        x,
+    ) for i, x in enumerate(results)],
+))
 
 with open("results.json", "w") as f:
-    f.write(json.dumps(data, indent=4))
+    f.write(json.dumps(existing_data, indent=4))
